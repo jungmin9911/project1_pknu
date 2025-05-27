bf57b20f48aabbccc82268391f45cd29fa4254b7
jungmin9911 <jmahn1819@gmail.com>
2025-05-27 17:30:50 +0900

    0527
    으으윽

---
+++ todo/src/main/java/com/todo/todo/controller/MyController.java
@@ -0,0 +1,15 @@
+package main.java.com.todo.todo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotaion.GetMapping;
+
+// import java.util.List;
+
+@Controller
+public class MyController {
+    @GetMapping("/")
+    public String mainPage(Model model){
+        return "index";
+    }
+}
